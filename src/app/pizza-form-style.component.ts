89909ca11810b48938f6371f78358f09c8fc3ccf
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-02-09 21:00:04 +0100

    Add validation

--- src/app/pizza-form-style.component.ts
+++ src/app/pizza-form-style.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pizza-form-style',
@@ -12,23 +14,50 @@ import { FormControl, Validators, FormGroup } from '@angular/forms';
           {{option.label}}
         </option>
       </select>
+      <div *ngIf="showSauces">
+        <label *ngFor="let sauce of availableSauces">
+          <input type="radio" formControlName="sauce" [value]="sauce.id">
+          {{sauce.label}}
+        </label>
+      </div>
     </div>
   `,
   styles: []
 })
-export class PizzaFormStyleComponent {
+export class PizzaFormStyleComponent implements OnInit, OnDestroy {
+  destroyed$ = new Subject();
+
   styleInfo = new FormGroup({
     style: new FormControl('', [Validators.required]),
+    sauce: new FormControl('', [Validators.required]),
   });
 
   availableStyles = [
-    { id: 0, label: 'Roman' },
-    { id: 2, label: 'Neapolitan' },
-    { id: 3, label: 'NY Style' },
-    { id: 4, label: 'Chicago' },
+    { id: 'style-1', needsSauceChoice: true, label: 'Roman' },
+    { id: 'style-2', needsSauceChoice: true, label: 'Neapolitan' },
+    { id: 'style-3', needsSauceChoice: true, label: 'NY Style' },
+    { id: 'style-4', needsSauceChoice: false, label: 'Chicago Deep Dish' },
   ];
+
+  availableSauces = [
+    { id: 'sauce-123', label: 'Tomato' },
+    { id: 'sauce-456', label: 'Creme Fraiche' },
+  ];
+
+  showSauces?: boolean;
 
   @Input() set parent(val: FormGroup) {
     val.addControl('pizzaTitle', this.styleInfo);
+  }
+
+  ngOnInit(): void {
+    this.styleInfo.get('style')?.valueChanges
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(v => this.showSauces = this.availableStyles.find(s => s.id === v)?.needsSauceChoice);
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
